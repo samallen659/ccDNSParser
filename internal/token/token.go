e0093fe17a04bdb61bdb0ef643558d7abfed6fb7
Sam Allen <samallen659@gmail.com>
2024-04-18 19:47:19 +0100

    adds tokens

---
+++ internal/token/token.go
@@ -0,0 +1,24 @@
+package token
+
+const (
+	ILLEGAL = "illegal"
+	EOF     = "eof"
+
+	INT    = "INT"
+	STRING = "STRING"
+
+	COLON = ":"
+	COMMA = ","
+
+	LPAREN   = "("
+	RPAREN   = ")"
+	LBRACE   = "{"
+	RBRACE   = "}"
+	LBRACKET = "["
+	RBRACKET = "]"
+
+	TRUE  = "true"
+	FALSE = "false"
+
+	MINUS = "-"
+)
