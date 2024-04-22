6f97a181f512d3dd55677353ea44ad7ff5761418
Sam Allen <samallen659@gmail.com>
2024-04-22 19:39:08 +0100

    adds Lexer methods and helper functions for lexing

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -1,8 +1,63 @@
 package lexer
 
+import (
+	"github.com/samallen659/ccJSONParser/internal/token"
+)
+
 type Lexer struct {
-	input        []byte
+	input        string
 	position     int
 	readPosition int
 	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case ':':
+		tok = newToken(token.COLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case '[':
+		tok = newToken(token.LBRACKET, l.ch)
+	case ']':
+		tok = newToken(token.RBRACKET, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	}
+	l.readChar()
+	return tok
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
 }
