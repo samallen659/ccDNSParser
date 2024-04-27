c4610cab21ee231e12c21a239abc48b4fde66182
Sam Allen <samallen659@gmail.com>
2024-04-27 15:07:21 +0100

    adds lexer functionality for strings, ints, illegal & eof

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -1,6 +1,8 @@
 package lexer
 
 import (
+	"bytes"
+
 	"github.com/samallen659/ccJSONParser/internal/token"
 )
 
@@ -22,23 +24,33 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case ':':
-		tok = newToken(token.COLON, l.ch)
+		tok = newToken(token.COLON, []byte{l.ch})
 	case '(':
-		tok = newToken(token.LPAREN, l.ch)
+		tok = newToken(token.LPAREN, []byte{l.ch})
 	case ')':
-		tok = newToken(token.RPAREN, l.ch)
+		tok = newToken(token.RPAREN, []byte{l.ch})
 	case '{':
-		tok = newToken(token.LBRACE, l.ch)
+		tok = newToken(token.LBRACE, []byte{l.ch})
 	case '}':
-		tok = newToken(token.RBRACE, l.ch)
+		tok = newToken(token.RBRACE, []byte{l.ch})
 	case '[':
-		tok = newToken(token.LBRACKET, l.ch)
+		tok = newToken(token.LBRACKET, []byte{l.ch})
 	case ']':
-		tok = newToken(token.RBRACKET, l.ch)
+		tok = newToken(token.RBRACKET, []byte{l.ch})
 	case '-':
-		tok = newToken(token.MINUS, l.ch)
+		tok = newToken(token.MINUS, []byte{l.ch})
 	case ',':
-		tok = newToken(token.COMMA, l.ch)
+		tok = newToken(token.COMMA, []byte{l.ch})
+	case '"':
+		tok = newToken(token.STRING, l.readString())
+	case 0:
+		tok = token.Token{Type: token.EOF, Literal: "EOF"}
+	default:
+		if isDigit(l.ch) {
+			tok = newToken(token.INT, l.readNumber())
+		} else {
+			tok = token.Token{Type: token.ILLEGAL, Literal: "ILLEGAL"}
+		}
 	}
 	l.readChar()
 	return tok
@@ -54,10 +66,40 @@ func (l *Lexer) readChar() {
 	l.readPosition++
 }
 
-func newToken(tokenType token.TokenType, ch byte) token.Token {
+func (l *Lexer) readString() []byte {
+	var out bytes.Buffer
+
+	for l.input[l.readPosition] != '"' {
+		l.readChar()
+		out.WriteByte(l.ch)
+	}
+	l.readChar()
+
+	return out.Bytes()
+}
+
+func (l *Lexer) readNumber() []byte {
+	var out bytes.Buffer
+
+	for isDigit(l.ch) {
+		out.WriteByte(l.ch)
+		if !isDigit(l.input[l.readPosition]) {
+			break
+		}
+		l.readChar()
+	}
+
+	return out.Bytes()
+}
+
+func newToken(tokenType token.TokenType, ch []byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
 func isLetter(ch byte) bool {
 	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
